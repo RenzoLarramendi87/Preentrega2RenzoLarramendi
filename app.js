28290e2bfe211295891243f3f7cf7aa154694994
renzolarramendi <renhatecrew87@gmail.com>
2023-11-20 03:38:28 -0300

    carrito

--- app.js
+++ app.js
@@ -1,44 +1,7 @@
 const shopContent = document.getElementById("shopContent");
+const verCarrito = document.getElementById("verCarrito");
+const modalContainer = document.getElementById("modal-container");
 
-
-const productos = [
-    {
-        id:1,
-        nombre: "Capuchino", 
-        precio: 100,
-        img:
-        "https://www.istockphoto.com/es/foto/filtro-de-caf%C3%A9-gm1033432408-276752868?utm_source=pixabay&utm_medium=affiliate&utm_campaign=SRP_image_sponsored&utm_content=https%3A%2F%2Fpixabay.com%2Fes%2Fimages%2Fsearch%2Fcaf%25C3%25A9%2F&utm_term=caf%C3%A9",
-
-    },
-        
-    {
-        id:2,
-        nombre: "Espresso", 
-        precio: 120,
-        img:
-        "https://www.istockphoto.com/es/foto/concepto-de-caf%C3%A9-turco-taza-de-caf%C3%A9-con-granos-de-caf%C3%A9-sobre-fondo-de-madera-gm1301467250-393504774?utm_source=pixabay&utm_medium=affiliate&utm_campaign=SRP_image_sponsored&utm_content=https%3A%2F%2Fpixabay.com%2Fes%2Fimages%2Fsearch%2Fcaf%25C3%25A9%2F&utm_term=caf%C3%A9",
-
-    },
-
-
-    {
-        id:3,
-        nombre: "Mocchiato", 
-        precio: 160,
-        img:
-        "https://pixabay.com/es/photos/caf%C3%A9-bol%C3%ADgrafo-computadora-port%C3%A1til-2306471/",
-
-
-    },
-       
-    {   
-        id:4,
-        nombre: "Mocca", 
-        precio: 180,
-        img:
-        "https://pixabay.com/es/photos/taza-de-caf%C3%A9-caf%C3%A9-latido-del-coraz%C3%B3n-2317201/",
-    },    
-];
 
 let carrito = [];
 
@@ -55,11 +18,70 @@ productos.forEach((product)=> {
     shopContent.append(content);
 
 
-    let comprar = document.createElement("button")
-    comprar.innerText = "comprar"
-    comprar.className = "comprar"
+    let comprar = document.createElement("button");
+    comprar.innerText = "comprar";
+    comprar.className = "comprar";
 
     content.append(comprar);
 
 
+    comprar.addEventListener("click", () =>{
+        carrito.push({
+            id: product.id,
+            img: product.img,
+            nombre: product.nombre,
+            precio: product.precio,
+
+        });
+        console.log(carrito);
+    });
+
+});
+
+
+verCarrito.addEventListener("click", () =>{
+    modalContainer.innerHTML = "";
+    modalContainer.style.display = "flex";
+    const modalHeader = document.createElement("div");
+    modalHeader.className = "modal-header";
+    modalHeader.innerHTML = `
+        <h1 class="modal-header-title">Carrito.</h1>
+
+    `;
+    modalContainer.append(modalHeader);
+
+    const modalbutton = document.createElement("h1");
+    modalbutton.innerText = "X";
+    modalbutton.className = "modal-header-button";
+
+
+    modalbutton.addEventListener("click", () => {
+        modalContainer.style.display = "none"
+    });
+
+    modalHeader.append(modalbutton);
+
+    carrito.forEach((product) => {
+        let carritoContent = document.createElement("div");
+        carrito.className = "modal-content";
+        carrito.innerHTML = `
+          <img src="${product.img}">
+          <h3>${product.nombre}</h3>  
+          <p>${product.precio} $</p>
+        
+        `;
+
+
+        modalContainer.append(carritoContent);
+
+
+    });  
+
+
+    const total = carrito.reduce((acc, el) => acc + el.precio, 0);
+
+    const totalBuying = document.createElement("div");
+    totalBuying.className = "total-content";
+    totalBuying.innerHTML = `total a pagar: ${total}$`;
+    modalContainer.append(totalBuying);
 });
---
+++ products.js
@@ -0,0 +1,38 @@
+const productos = [
+    {
+        id:1,
+        nombre: "Capuchino", 
+        precio: 100,
+        img:
+        "https://www.istockphoto.com/es/foto/filtro-de-caf%C3%A9-gm1033432408-276752868?utm_source=pixabay&utm_medium=affiliate&utm_campaign=SRP_image_sponsored&utm_content=https%3A%2F%2Fpixabay.com%2Fes%2Fimages%2Fsearch%2Fcaf%25C3%25A9%2F&utm_term=caf%C3%A9",
+
+    },
+        
+    {
+        id:2,
+        nombre: "Espresso", 
+        precio: 120,
+        img:
+        "https://www.istockphoto.com/es/foto/concepto-de-caf%C3%A9-turco-taza-de-caf%C3%A9-con-granos-de-caf%C3%A9-sobre-fondo-de-madera-gm1301467250-393504774?utm_source=pixabay&utm_medium=affiliate&utm_campaign=SRP_image_sponsored&utm_content=https%3A%2F%2Fpixabay.com%2Fes%2Fimages%2Fsearch%2Fcaf%25C3%25A9%2F&utm_term=caf%C3%A9",
+
+    },
+
+
+    {
+        id:3,
+        nombre: "Mocchiato", 
+        precio: 160,
+        img:
+        "https://pixabay.com/es/photos/caf%C3%A9-bol%C3%ADgrafo-computadora-port%C3%A1til-2306471/",
+
+
+    },
+       
+    {   
+        id:4,
+        nombre: "Mocca", 
+        precio: 180,
+        img:
+        "https://pixabay.com/es/photos/taza-de-caf%C3%A9-caf%C3%A9-latido-del-coraz%C3%B3n-2317201/",
+    },    
+];
